112a46fd7c523e549321ef222af8cdbd2ee1a1ae
Fernando Benjamin <4045639+kanuku@users.noreply.github.com>
2021-11-07 19:43:54 +0100

    Adding spring-boot project

---
+++ src/main/java/com/github/attilabot/attilabot/AttilaBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.attilabot.attilabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttilaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttilaBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/attilabot/attilabot/AttilaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.attilabot.attilabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttilaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
